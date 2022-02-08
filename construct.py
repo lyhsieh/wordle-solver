b6ccececa31eaab64c39a57a9deef8f65e335670
leothebestcoder <leoshieh0hn@gmail.com>
2022-02-08 10:59:58 +0800

    construct original wordbase

--- construct.py
+++ construct.py
@@ -1,14 +1,13 @@
 def alphabet(ch: str) -> bool:
     return True if ord('a') <= ord(ch) <= ord('z') else False
 
-fh = open('/usr/share/dict/words')
+fh = open('/usr/share/dict/words', 'r')
 fh2 = open('wordlist.txt', 'w')
 
-five = []
 for w in fh.readlines():
     new_w = w[:-1] if w[-1] == '\n' else w
     if len(new_w) == 5 and all([alphabet(ch) for ch in new_w]):
-        five.append(new_w)
         fh2.write(new_w + '\n')
+        
 fh.close()
 fh2.close()
